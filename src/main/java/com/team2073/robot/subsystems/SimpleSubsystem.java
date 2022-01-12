1062026066100a4e2f58f32e3b49490dcd536cd4
JaydenDhaliwal <jaydhaliwal06@gmail.com>
2022-01-11 20:43:38 -0800

    Working good model without pulse PLEASE DON'T GIVE ME THE ANSWER BOYUAN.

--- src/main/java/com/team2073/robot/subsystems/SimpleSubsystem.java
+++ src/main/java/com/team2073/robot/subsystems/SimpleSubsystem.java
@@ -11,7 +11,7 @@ public class SimpleSubsystem implements AsyncPeriodicRunnable {
     private final ApplicationContext appCtx = ApplicationContext.getInstance();
     private final CANSparkMax motor = appCtx.getMotor();
     private double originalPosition = 0;
-    private SimpleSubsystemState currentState = SimpleSubsystemState.STICK;
+    private SimpleSubsystemState currentState = SimpleSubsystemState.TEMP;
     private double output = 0;
     public double originalOutput = 0;
     public Timer timer = new Timer();
@@ -28,42 +28,49 @@ public class SimpleSubsystem implements AsyncPeriodicRunnable {
                 break;
             case HALF_POWER:
                 output = 0.5;
-                System.out.println("A");
+                System.out.println(output);
+
+                /*
                 if (appCtx.getController().getRawAxis(2) != 0 || appCtx.getController().getRawAxis(3) != 0) {
                     double totalChange = appCtx.getController().getRawAxis(3) - appCtx.getController().getRawAxis(2);
                     output = (output * totalChange) + output;
                 }
+                */
+
                 break;
             case STICK:
                 output = -appCtx.getController().getY();
+
+
                 if (appCtx.getController().getRawAxis(2) != 0 || appCtx.getController().getRawAxis(3) != 0) {
                     double totalChange = appCtx.getController().getRawAxis(3) - appCtx.getController().getRawAxis(2);
                     output = (output * totalChange) + output;
                 }
 
+
                 break;
             case PULSE:
-                if(timer.getElapsedTime() == 0){
+                if (timer.getElapsedTime() == 0) {
                     timer.start();
                 }
-                if(((int)timer.getElapsedTime() / 1000) % 2 == 0){
+                if (((int) timer.getElapsedTime() / 1000) % 2 == 0) {
                     output = 0.25;
-                }else{
+                } else {
                     output = 0;
                 }
                 break;
             case RESET:
                 System.out.println(motor.getEncoder().getPosition());
-                if (!((int)motor.getEncoder().getPosition() < 50 && (int)motor.getEncoder().getPosition() >-50)) {
+                if (!((int) motor.getEncoder().getPosition() < 50 && (int) motor.getEncoder().getPosition() > -50)) {
                     System.out.println(motor.getEncoder().getPosition());
-                    if((int) motor.getEncoder().getPosition() > 50) {
+                    if ((int) motor.getEncoder().getPosition() > 50) {
                         output = -0.2;
-                        if(appCtx.getController().getY() < -0.2 || appCtx.getController().getY() > 0.2){
+                        if (appCtx.getController().getY() < -0.2 || appCtx.getController().getY() > 0.2) {
                             output = -appCtx.getController().getY();
                         }
-                    }else if((int)motor.getEncoder().getPosition() < -50) {
+                    } else if ((int) motor.getEncoder().getPosition() < -50) {
                         output = 0.2;
-                        if(appCtx.getController().getY() < -0.2 || appCtx.getController().getY() > 0.2){
+                        if (appCtx.getController().getY() < -0.2 || appCtx.getController().getY() > 0.2) {
                             output = -appCtx.getController().getY();
                         }
                     }
@@ -73,39 +80,43 @@ public class SimpleSubsystem implements AsyncPeriodicRunnable {
                 }
                 break;
             case CRUISE:
-                if(originalOutput == 0){
+                if (originalOutput == 0) {
                     originalOutput = output;
                 }
                 System.out.println("original output: " + originalOutput);
-                if(originalOutput > 0) {
+                System.out.println("output " + output);
+                System.out.println("appCtx.getController().getY() " + appCtx.getController().getY());
+                if (originalOutput > 0) {
                     if (appCtx.getController().getY() > originalOutput) {
                         output = appCtx.getController().getY();
                     } else {
                         output = originalOutput;
                     }
-                }else{
-                    if(appCtx.getController().getY() < originalOutput){
+                } else {
+                    if (appCtx.getController().getY() < originalOutput) {
                         output = appCtx.getController().getY();
-                    }else{
+                    } else {
                         output = originalOutput;
                     }
                 }
                 break;
             case REVOLUTION:
-                if(originalPosition == 0){
+                if (originalPosition == 0) {
                     originalPosition = motor.getEncoder().getPosition();
                 }
-                if(originalPosition + 3000 > motor.getEncoder().getPosition()){
+                if (originalPosition + 3000 > motor.getEncoder().getPosition()) {
                     output = 0.5;
                     System.out.println(motor.getEncoder().getPosition());
-                }
-                else{System.out.println("Done"); output = 0;break;
+                } else {
+                    System.out.println("Done");
+                    output = 0;
+                    break;
                 }
                 break;
             default:
                 output = 0;
                 break;
-            }
+        }
             if (output >= 0.8) {
                 output = 0.8;
             } else if (output < 0.2 && output > -0.2) {
@@ -113,6 +124,8 @@ public class SimpleSubsystem implements AsyncPeriodicRunnable {
             } else if (output <= -0.8) {
                 output = -0.8;
             }
+            System.out.println("Right before set: " + output);
+            System.out.println("Current State " + currentState);
             motor.set(output);
         }
 
@@ -126,6 +139,7 @@ public class SimpleSubsystem implements AsyncPeriodicRunnable {
 
     public enum SimpleSubsystemState {
         STOP,
+        TEMP,
         HALF_POWER,
         STICK,
         PULSE,
